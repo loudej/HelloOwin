184e6378cb6492755ec43ac97d6b96f0b122e844
Louis DeJardin <louis.dejardin@gmail.com>
2013-04-22 21:11:40 -0700

    First commit

---
+++ Readme.md
@@ -0,0 +1,12 @@
+
+
+Hello, OWIN!
+
+Try 
+
+git clone https://github.com/loudej/HelloOwin.git
+cd HelloOwin\src\HelloOwin
+msbuild
+go
+
+Good luck!
---
+++ src/HelloOwin/Startup.cs
@@ -0,0 +1,25 @@
+﻿using Owin;
+
+namespace HelloOwin
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // example of a filter - writeline each request
+            app.UseFilter(req => req.TraceOutput.WriteLine(
+                "{0} {1}{2} {3}",
+                req.Method,
+                req.PathBase,
+                req.Path,
+                req.QueryString));
+
+            // example of a handler - all paths reply Hello, Owin!
+            app.UseHandler(async (req, res) =>
+            {
+                res.ContentType = "text/plain";
+                await res.WriteAsync("Hello, OWIN!");
+            });
+        }
+    }
+}
