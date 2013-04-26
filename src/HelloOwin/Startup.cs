c114e70cbb88c18c9350b4c63d7689212ed9b4c8
Louis DeJardin <louis.dejardin@gmail.com>
2013-04-26 00:14:10 -0700

    Updating to show logger and data protection things

--- src/HelloOwin/Startup.cs
+++ src/HelloOwin/Startup.cs
@@ -1,4 +1,6 @@
-﻿using Owin;
+﻿using Microsoft.Owin.Logging;
+using Microsoft.Owin.Security.DataProtection;
+using Owin;
 
 namespace HelloOwin
 {
@@ -6,13 +8,20 @@ namespace HelloOwin
     {
         public void Configuration(IAppBuilder app)
         {
+            var logger = app.CreateLogger(GetType());
+
+            app.SetDataProtectionProvider(new SharedSecretDataProtectionProvider(
+                "af98j3pf98ja3fdopa32hr !!!! DO NOT USE THIS STRING IN YOUR APP !!!!",
+                "AES",
+                "HMACSHA256"));
+
             // example of a filter - writeline each request
-            app.UseFilter(req => req.TraceOutput.WriteLine(
+            app.UseFilter(req => logger.WriteInformation(string.Format(
                 "{0} {1}{2} {3}",
                 req.Method,
                 req.PathBase,
                 req.Path,
-                req.QueryString));
+                req.QueryString)));
 
             // example of a handler - all paths reply Hello, Owin!
             app.UseHandler(async (req, res) =>
